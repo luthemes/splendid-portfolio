b73afd2cc545e17c2da0d256d90a44ce7a7c42e3
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-10-31 02:01:44 -0700

    done

--- public/views/404.php
+++ public/views/404.php
@@ -2,11 +2,11 @@
 /**
  * Default 404 template
  *
- * @package   Initiator
- * @author    Benjamin Lu ( benlumia007@gmail.com )
- * @copyright Copyright (C) 2019-2021. Benjamin Lu
+ * @package   Splendid Portfolio
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright Copyright (C) 2017-2021. Benjamin Lu
  * @license   https://www.gnu.org/licenses/gpl-2.0.html
- * @link      https://github.com/benlumia007/initiator
+ * @link      https://luthemes.com/portfolio/splendid-portfolio
  */
 ?>
 <?php $engine = Benlumia007\Backdrop\App::resolve( 'view/engine' ); ?>
