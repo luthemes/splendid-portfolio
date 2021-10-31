7c89d811a7b319565b640f9ffabbcfb9e9015508
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-10-31 02:03:26 -0700

    done

--- public/views/archive.php
+++ public/views/archive.php
@@ -1,31 +1,37 @@
 <?php
 /**
- * Default index template
+ * Default archive template
  *
- * @package   Initiator
- * @author    Benjamin Lu ( benlumia007@gmail.com )
- * @copyright Copyright (C) 2019-2021. Benjamin Lu
+ * @package   Splendid Portfolio
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright Copyright (C) 2017-2021. Benjamin Lu
  * @license   https://www.gnu.org/licenses/gpl-2.0.html
- * @link      https://github.com/benlumia007/initiator
+ * @link      https://luthemes.com/portfolio/splendid-portfolio
  */
 ?>
 <?php $engine = Benlumia007\Backdrop\App::resolve( 'view/engine' ); ?>
 <?php $engine->display( 'header' ); ?>
 	<section id="content" class="site-content">
-		<div id="layout" class="<?php echo esc_attr( get_theme_mod( 'global_layout', 'no-sidebar' ) ); ?>">
+		<div id="global-layout" class="no-sidebar">
 			<main id="main" class="content-area">
-				<?php
-					if ( have_posts() ) :
-						while ( have_posts() ) : the_post();
-							$engine->display( 'content/archive'  );
-						endwhile;
-						the_posts_pagination();
-					else :
-							$engine->display( 'content/none' );
-					endif;
-				?>
+				<?php if ( ! is_front_page() ) { ?>
+					<header class="archive-header">
+						<?php the_archive_title( '<h1 class="archive-title">', '</h1>'); ?>
+						<?php the_archive_description( '<p class="archive-description">', '</p>'); ?>
+					</header>
+				<?php } ?>
+
+				<?php if ( have_posts() ) : ?>
+					<div class="loop">
+						<ul class="grid has-3-columns">
+							<?php while ( have_posts() ) : the_post(); ?>
+								<?php $engine->display( 'content/archive' ); ?>
+							<?php endwhile; ?>
+						</ul>
+						<?php the_posts_pagination(); ?>
+					</div>
+				<?php endif; ?>
 			</main>
-			<?php Benlumia007\Backdrop\Theme\Sidebar\display( 'sidebar', [ 'primary' ] ); ?>
 		</div>
 	</section>
 <?php $engine->display( 'footer' ); ?>
