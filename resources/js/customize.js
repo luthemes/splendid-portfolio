624bf61968caa44745c7a33d96378a2153f930ac
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-11-11 18:32:47 -0800

    make sure that all css and js scripts are loaded

---
+++ resources/js/customize.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function(){
+    wp.customize.controlConstructor['radio-image'] = wp.customize.Control.extend({
+        ready: function() {
+            var control = this;
+            var value = (undefined !== control.setting._value) ? control.setting._value : '';
+
+            this.container.on( 'change', 'input:radio', function() {
+                value = jQuery( this ).val();
+                control.setting.set( value );
+            });
+        }
+    });
+});
